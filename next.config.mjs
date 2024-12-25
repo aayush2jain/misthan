e84d22ba47f87effd15d92564a6d0907a3328de3
aayush jain <aayushjain1290@gmail.com>
2024-12-25 15:42:13 +0530

    kold

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
  domains: ['res.cloudinary.com','photos.google.com','www.freepik.com',"i.pinimg.com", 'img.freepik.com','https://m.media-amazon.com','https://images.unsplash.com','images.unsplash.com']
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
